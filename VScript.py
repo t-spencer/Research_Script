fd8be69f344aceb13197cfe520324b18ae055c68
Trent Spencer <trentspencer144@gmail.com>
2016-09-11 01:50:07 +0000

    Updating old code to new one

--- VScript.py
+++ VScript.py
@@ -1,7 +1,7 @@
 
 # coding: utf-8
 
-# In[5]:
+# In[ ]:
 
 #----------VERSION HISTORY-----------------------------------
 # Feb 22, 2016 - Created Initial file, got file to ingest into code (v0.1->v0.1.1), working to figure out
@@ -14,10 +14,15 @@
 # Mar 20, 2016 - Added code to create second plot, seems to work fine. Added comments for each line for
 # anybody who would like to use this in the future. Not too many other changes, upcoming work may include
 # creating reflectivity color table, then plotting. (v1.0->v1.0.1)
+
+# Mar 22, 2016 - Completely redid code, moved to class structure instead of one long definition. Will go back
+# and insert comments on lines again for whoever looks at this in the future. Having issues with the plots of 
+# the Composite Reflectivity and the Cold pool. Maybe need a new model run? More work this week on this issue,
+# will log in github issue tracker. (v1.0.1->v2.0)
 #------------------------------------------------------------------------------------------------------------------
 
 # Prints opening info
-print("\n Research Visualization Script v1.0.1 \n Author: Trent Spencer, Texas A&M University \n Python 3.5 \n See LICENSE for details of use \n See Github or Code for Version History \n")
+print("\n Research Visualization Script v2.0 \n Author: Trent Spencer, Texas A&M University \n Python 3.5 \n See LICENSE for details of use \n See Github or Code for Version History \n")
 
 # This section imports the necessary modules
 import netCDF4 as nc
@@ -26,36 +31,123 @@ import matplotlib.pyplot as plt
 import matplotlib.cm as cm
 import time
 from datetime import datetime
+from metpy.plots import ctables
 
 # Begin of actual script
-def Vis_script(time, ni, nj, nk):
-    '''This function pulls the data from the .nc file and returns the plotted images specified in the code.'''
-    data = nc.Dataset("../cm1out.nc") # opens file 
-    timedata = data.variables["time"][time] #pulls time
-    mxheli = data.variables['shs'][time, nj, ni] # pulls max updraft helicity
-    cold = data.variables['cph'][time, nj, ni] # pulls cold pool height
-    x = data.variables['xh'][ni]   # pulls x-coordinates
-    y = data.variables['yh'][nj]   # pulls y-coordinates
-    xall, yall = np.meshgrid(x, y) # creates grid for plotting data
-    fig1 = plt.figure(figsize = (25,25)) # creates figure
-    ax1 = fig1.add_subplot(121) # creates first plot
-    ax1.contourf(xall, yall, mxheli, cmap = cm.Blues) # makes filled contours in first plot displaying updraft helicity
-    ax2 = fig1.add_subplot(122) # creates second plot
-    ax2.contourf(xall, yall, cold, cmap = cm.Blues) # makes filled contours of cold pool height in second plot
-    return fig1  # returns the figure
+class ResPlot:
+    def __init__(self, filename):
+        self.filename = filename
+        self.to_plot = []
+        self.ni = 0
+        self.nj = 0
+        self.nk = 0
+        self.time = 0
+        
+    def getinfo(self):
+        self.ni = int(input("Enter ni value from namelist: "))
+        self.nj = int(input("Enter nj value from namelist: "))
+        self.nk = int(input("Enter nk value from namelist: "))
+        self.time = int(input("Enter time value from ncdump: "))
     
+    def whatplot(self):
+        names = ['Cold Pool Intensity', 'Cold Pool Height', 'Max Updraft Helicity', 'Reflectivity', 'max vert vorticity at lowest level', 'surface potential temperature', 'maximum updraft speed at 5 km AGL']
+        v = ['cpc','cph','shs','cref','svs','sfcth','sus']
+        validanswers = ['Y','y','Yes','yes', 'YES', "Sure", 'sure', 'OK', 'ok']
+        b = 0
+        for vari in names:
+            a = input("Plot " + vari + '? ')
+            if a in validanswers:
+                self.to_plot.append(v[b])
+                b = b + 1
+            else:
+                b = b + 1
+    
+    def plot(self):
+        '''Does all the plotting'''
+        data = nc.Dataset(self.filename)
+        x = data['xh'][range(self.ni)]
+        y = data['yh'][range(self.nj)]
+        xall, yall = np.meshgrid(x,y)
+        if 'cpc' in self.to_plot:
+            for t in range(self.time):
+                fig1 = plt.figure(figsize = (25,25) )
+                cpcdata = data['cpc'][t, range(self.nj), range(self.ni)]
+                ax1 = fig1.add_subplot(111)
+                ax1.contourf(xall, yall, cpcdata, cmap = cm.Blues)
+                ax1.set_title(str(data['time'][t]), fontsize = 30)
+                plt.savefig("cpc_" + str(t))
+                plt.close('all')
+                
+        if 'cph' in self.to_plot:
+            for t in range(self.time):
+                fig1 = plt.figure(figsize = (25,25) )
+                cpcdata = data['cph'][t, range(self.nj), range(self.ni)]
+                ax1 = fig1.add_subplot(111)
+                ax1.contourf(xall, yall, cpcdata, cmap = cm.Blues)
+                ax1.set_title(str(data['time'][t]), fontsize = 30)
+                plt.savefig("cph_" + str(t))
+                plt.close('all')
+        
+        if 'shs' in self.to_plot:
+            for t in range(self.time):
+                fig1 = plt.figure(figsize = (25,25) )
+                shsdata = data['shs'][t, range(self.nj), range(self.ni)]
+                ax1 = fig1.add_subplot(111)
+                ax1.contourf(xall, yall, shsdata, cmap = cm.Blues)
+                ax1.set_title(str(data['time'][t]), fontsize = 30)
+                plt.savefig("shs_" + str(t))
+                plt.close('all')
 
+        if 'cref' in self.to_plot:
+            for t in range(self.time):
+                fig1 = plt.figure(figsize = (25,25) )
+                crefdata = data['cref'][t, range(self.nj), range(self.ni)]
+                ax1 = fig1.add_subplot(111)
+                cmap = ctables.registry.get_colortable('NWSReflectivityExpanded')
+                ax1.contourf(xall, yall, crefdata, cmap = cmap)
+                ax1.set_title(str(data['time'][t]), fontsize = 30)
+                plt.savefig("cref_" + str(t))
+                plt.close('all')
+                
+        if 'svs' in self.to_plot:
+            for t in range(self.time):
+                fig1 = plt.figure(figsize = (25,25) )              
+                svsdata = data['svs'][t, range(self.nj), range(self.ni)]
+                ax1 = fig1.add_subplot(111)
+                ax1.contourf(xall, yall, svsdata, cmap = cm.jet)
+                ax1.set_title(str(data['time'][t]), fontsize = 30)
+                plt.savefig("svs_" + str(t))
+                plt.close('all')
+                
+        if 'sfcth' in self.to_plot:
+            for t in range(self.time):
+                fig1 = plt.figure(figsize = (25,25) )              
+                thdata = data['th'][t, 0, range(self.nj), range(self.ni)]
+                ax1 = fig1.add_subplot(111)
+                ax1.contourf(xall, yall, thdata, cmap = cm.Blues)
+                ax1.set_title(str(data['time'][t]), fontsize = 30)
+                plt.savefig("sfcth_" + str(t))
+                plt.close('all')
+
+        if 'sus' in self.to_plot:
+            for t in range(self.time):
+                fig1 = plt.figure(figsize = (25,25) )                
+                susdata = data['sus'][t, range(self.nj), range(self.ni)]
+                ax1 = fig1.add_subplot(111)
+                ax1.contourf(xall, yall, susdata, cmap = cm.Blues)
+                ax1.set_title(str(data['time'][t]), fontsize = 30)
+                plt.savefig("susth_" + str(t))
+                plt.close('all')
+                 
+a = ResPlot('../cm1out.nc')
+a.whatplot()
+a.getinfo()
 print('\n Script began at:', datetime.utcnow(), 'UTC.')
 sttime = time.time()
-for t in range(0,45): # loops over each time step
-    Vis_script(t,range(0,200),range(0,200),range(0,40))
-    plt.savefig(str(t) + "cm1svs.png") # saves images as .png (Change to .pdf?)
-    plt.close('all') # closes open figure to keep memory usage low (~160 mb RAM as of 4/20/16)
+a.plot()
 entime = time.time()
-print("Script took ", (entime - sttime), " seconds to run. \n")
-
-
-# In[ ]:
-
-
+if entime - sttime < 60:
+    print("\n Script took ", (entime - sttime), " seconds to run. \n")
+else:
+    print("\n Script took ", (entime - sttime)/60 , " minutes to run. \n")
 
